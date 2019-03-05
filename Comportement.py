7405637e611046ecc2d0a759cc1fba807623d063
Noblessse <47557105+Noblessse@users.noreply.github.com>
2019-03-05 09:36:52 +0100

    Comportement
    Comportement de l'accélération de la voiture en fonction des différents paramèters

---
+++ Comportement.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Comportement:
+    
+    def init(self, a_max, a_min, temps_reaction, longueur):
+        self.a_max = a_max
+        self.a_min = a_min
+        self.temps_reaction = temps_reaction
+        self.longueur = longueur
+        
+    def calcul_acceleration:
+        
+        Tau = vitesse_limite / self.a_max
+
+        temps_securite = self.temps_reaction
+
+        distance_securite = temps_securite * v_i + self.longueur
+        delta_x = distance_securite - distance
+        
+        if delta_x < 0:
+            vitesse_vehicule = vitesse_limite
+        else:  
+            vitesse_vehicule = v_j * (1 - delta_x / distance_securite)
+
+
+# In[ ]:
+
+
+
+
